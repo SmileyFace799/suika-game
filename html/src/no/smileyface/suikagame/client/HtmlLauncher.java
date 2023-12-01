3cd7bae5e8181fe029482e9e6d2ef3db8f4476f6
SmileyFace799 <Henrikgran85@gmail.com>
2023-12-01 23:32:59 +0100

    Created LibGDX project

---
+++ html/src/no/smileyface/suikagame/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package no.smileyface.suikagame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import no.smileyface.suikagame.SuikaGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SuikaGame();
+        }
+}
